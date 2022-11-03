c069a1a14a3386e74fe65d2df4e7ab02460fa29e
Firstname Lastname <adress@domain.com>
2022-11-03 15:48:07 +0100

    Enabled VM option "--nat-localhostreachable1"

--- run-packer-build.sh
+++ run-packer-build.sh
@@ -6,6 +6,6 @@ if [ -z "${VAGRANT_CLOUD_TOKEN}" -a -z "${OPTIONS}" ]; then
     OPTIONS="-except=vagrant-cloud"
 fi
 
-OPTIONS="${OPTIONS} -var iso_name=$(curl https://mirror.rackspace.com/archlinux/iso/latest/sha1sums.txt | head -n 1 | awk '{print $2}')"
+OPTIONS="${OPTIONS} -var iso_name=$(curl https://mirror.rackspace.com/archlinux/iso/latest/sha256sums.txt | head -n 1 | awk '{print $2}')"
 
 packer build -force -var 'headless=false' ${OPTIONS} $(readlink -f $0 | xargs dirname)/archlinux.json
